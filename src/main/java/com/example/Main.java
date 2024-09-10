bc2b7b11037cafb50ff72c503a23acf7332ca3ba
JuanOquendoweb <ojuan58870@gmail.com>
2024-09-09 19:31:35 -0500

    c2

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -1,7 +1,44 @@
 package com.example;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
        
+    Scanner cantidadCompra = new Scanner(System.in);
+    System.out.println("Ingrese la cantidad de compra: ");
+    double valor1 = cantidadCompra.nextDouble();
+
+    cantidadCompra.close();
+
+    int puntosAcumulados = (int) (valor1 / 10);
+
+    System.out.println("puntos acumulados: " + puntosAcumulados);
+
+    // niveles
+
+    String nivelFidelizacion;
+
+    if (puntosAcumulados <= 100) {
+      nivelFidelizacion = "bronce";
+    } else if (puntosAcumulados <= 500) {
+      nivelFidelizacion = "plata";
+    } else if (puntosAcumulados <= 1000) {
+      nivelFidelizacion = "oro";
+    } else {
+      nivelFidelizacion = "platino";
     }
-}
+
+    System.out.println("Tu nivel de fidelizacion es: " + nivelFidelizacion);
+
+    // Descuentos
+
+    if (nivelFidelizacion.equals("oro") || nivelFidelizacion.equals("platino")) {
+      System.out.println("Felicidades has ganado un descuento del 10% en tus proximas compras");
+
+    }
+   if (nivelFidelizacion.equals("platino")) {System.out.println("Felicidades has ganado un cupon para tu proximo producto");
+    
+   }
+  }
+    }
